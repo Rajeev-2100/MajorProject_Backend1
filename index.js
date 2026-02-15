22da6f5c8bc092c4be7047754a7e0c8b4a1d1545
Rajeev Rawat <rajeevrawat2100@email.com>
2026-02-15 23:34:34 +0530

    fix: the get wishlist data are not populate because not send the key

--- index.js
+++ index.js
@@ -69,11 +69,7 @@ app.get("/api/products/category/:CategoryId", async (req, res) => {
     console.log(product);
     if (product) {
       return res.status(200).json({ data: product });
-<<<<<<< HEAD
     } else {
-=======
-    }else{
->>>>>>> 7e5de8f77d04eb3777937ec15614b858158c3bcf
       res.status(404).json({ error: "This product Id not found" });
     }
   } catch (error) {
@@ -86,11 +82,7 @@ app.get("/api/products/:productId", async (req, res) => {
     const product = await getProductDetailByProductId(req.params.productId);
     if (product) {
       return res.status(201).json({ data: product });
-<<<<<<< HEAD
     } else {
-=======
-    }else{
->>>>>>> 7e5de8f77d04eb3777937ec15614b858158c3bcf
       res.status(404).json({ error: "This product Id not found" });
     }
   } catch (error) {
@@ -128,12 +120,6 @@ app.post("/category", async (req, res) => {
     } else {
       res.status(404).json({ error: "Category not created" });
     }
-<<<<<<< HEAD
-=======
-    else{
-      res.status(404).json({ error: "Category not created" });
-    }
->>>>>>> 7e5de8f77d04eb3777937ec15614b858158c3bcf
   } catch (error) {
     res
       .status(500)
@@ -348,7 +334,7 @@ app.post("/api/wishlist/:productId", async (req, res) => {
 
 async function getWishListData(){
   try {
-    const wishlist = await Wishlist.find().populate()
+    const wishlist = await Wishlist.find().populate('product')
     return wishlist
   } catch (error) {
     throw error
